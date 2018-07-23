476deb2475af9f76b503950cf1eb18f20563068d
ETSGitHub <etschulte@gmail.com>
2018-07-23 17:52:23 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -3,5 +3,12 @@ mv delivery/bison_Burgers meat
 mv delivery/carrot_cake.txt dessert
 mv delivery/fage_yogurt.txt dairy
 mv delivery/flourless_cake.txt dessert
-mv delivery/garlic.txt vegetable/garlic.txt
-mv delivery/goldfish.txt snax/goldfish.txt
+mv delivery/garlic.txt veggies
+mv delivery/goldfish.txt snax
+mv delivery/grapples.txt fruit
+mv delivery/hotdogs.txt meat
+mv delivery/icecream.txt dessert
+mv delivery/lamb_ribs.txt meat
+mv delivery/land_o_lakes_butter.txt dairy
+mv delivery/lean_cuisine.txt freezer
+mv delivery/leeks.txt veggies
