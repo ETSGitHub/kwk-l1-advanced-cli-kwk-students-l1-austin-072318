fae23e385cd5a93be29fe143824acfe694855443
ETSGitHub <etschulte@gmail.com>
2018-07-23 17:57:11 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -12,5 +12,16 @@ mv delivery/lamb_ribs.txt meat
 mv delivery/land_o_lakes_butter.txt dairy
 mv delivery/lean_cuisine.txt freezer
 mv delivery/leeks.txt veggies
-my delivery/milk.txt dairy
-my delivery/
+mv delivery/milk.txt dairy
+mv delivery/mint_milano_cookies.txt dessert
+mv delivery/onion.txt veggies
+mv delivery/oreos.txt dessert
+mv delivery/potatoes.txt veggies
+mv delivery/salmon_steak.txt meat
+mv delivery/tiramisu_cake.txt dessert
+mv delivery/tomatoes.txt veggies
+mv delivery/trail_mix.txt snax
+mv delivery/turkey_breat.txt meat
+mv delivery/velveeta_cheese.txt dairy
+mv delivery/watermelon.txt fruit
+mv delivery/
