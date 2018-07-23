85d17794b3e2cc76e37e842663291313afe9feb2
ETSGitHub <etschulte@gmail.com>
2018-07-23 17:35:43 +0000

    automatically backed up by learn

---
+++ stock_supermarket.sh
@@ -0,0 +1,9 @@
+mv delivery/apples fruit/apples
+mv delivery/bison_Burgers meat/bison_Burgers
+mv delivery/brie.txt dairy/brie.txt
+mv delivery/broccoli.txt veggies/broccoli.txt
+mv delivery/carrot_cake desssert/carrot_cake
+mv delivery/carrots.txt veggies/carrots.txt
+mv delivery/eggo_waffles.txt freezer/eggo_waffles.txt
+mv delivery/fage_yogurt.txt dairy/mv delivery/.txt
+mv delivery/fig_bars.txt snax/fig_bars.txt
